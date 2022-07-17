c7003a6277547342522e27ef74e3f6239080cf0b
Austin Shelby <helloaustinshelby@gmail.com>
2022-07-17 20:49:57 +0200

    netlify rewrites

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  rewrites: async () => [
-    {
-      source: "/countries",
-      destination: "/countries.json",
-    },
-    {
-      source: "/countries/:iso",
-      destination: "/:iso.json",
-    },
-  ],
 };
 
 module.exports = nextConfig;
